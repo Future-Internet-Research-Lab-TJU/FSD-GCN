4efb7c2741514f3f8a0923a2a058236ab4f7571c
Future-Internet-Research-Lab-TJU <zli@tju.edu.cn>
2025-10-23 19:41:04 +0800

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -4,5 +4,6 @@ import subprocess
 folder = "./export_lp/test"
 for file in os.listdir(folder):
     if file.endswith('.py'):
-        print(f"执行 {file}...")
+        print(f"{file}...")
+
         subprocess.run(["python", os.path.join(folder, file)])
